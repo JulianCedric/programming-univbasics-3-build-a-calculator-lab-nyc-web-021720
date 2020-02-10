860a3d5ccedbf18734f9126ea4d60d24a1b5ad08
Julian Pormentilla <julian.pormentilla@gmail.com>
2020-02-10 14:23:02 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,28 @@
 def addition(num1, num2)
-
+  sum = num1 + num2
+  return sum 
 end
 
 def subtraction(num1, num2)
-
+  difference = num1 - num2
+  return difference 
 end
 
 def division(num1, num2)
-
+  quotient = num1 / num2
+  return quotient
 end
 
 def multiplication(num1, num2)
-
+  product = num1 * num2
+  return product
 end
 
-def modulo(num1, num2)
+# def modulo(num1, num2)
+#   quotient_remainder = (num1.round / num2.round) - (num1 / num2)
+#   return quotient_remainder
+# end
 
-end
+# def square_root(num)
 
-def square_root(num)
-
-end
+# end
